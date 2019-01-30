eb7362793c4c1901beba81a42059c360f49fc04c
Jan Machacek <janmachacek97@gmail.com>
2019-01-30 22:26:33 +0000

    add intro page

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,55 @@
 import React from "react";
+import "../styles/Intro.css";
+import cSharp from "../images/cSharp.png";
+import cpp from "../images/cpp.png";
 
 const Intro = () => {
   return (
-    <section>
-      <h1>Intro</h1>
+    <section className="intro">
+      <div className="intro-class">
+        <span>
+          <i className="fas fa-code" />
+        </span>
+        <h1>Jan Machacek</h1>
+        <p>Full stack Developer</p>
+      </div>
+      <div className="icons">
+        <div>
+          <i class="fab fa-html5" />
+          <span>HTML5</span>
+        </div>
+        <div>
+          <i class="fab fa-js" />
+          <span>JavaScript</span>
+        </div>
+        <div>
+          <i class="fab fa-css3-alt" />
+          <span>CSS</span>
+        </div>
+        <div>
+          <i class="fas fa-database" />
+          <span>PostgreSQL</span>
+        </div>
+        <div>
+          <i class="fas fa-database" />
+          <span>MongoDB</span>
+        </div>
+        <div>
+          <i class="fas fa-server" />
+          <span>Express</span>
+        </div>
+        <div>
+          <img src={cSharp} alt="" />
+          <span>.NET</span>
+        </div>
+        <div>
+          <img src={cpp} alt="" />
+          <span>C++</span>
+        </div>
+
+        <i class="fab fa-node" />
+        <i class="fab fa-react" />
+      </div>
     </section>
   );
 };
