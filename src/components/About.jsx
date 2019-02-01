ed359503387a08ec27b49458bc8300432e2e56f1
Jan Machacek <janmachacek97@gmail.com>
2019-02-01 20:18:57 +0000

    add styling to about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import "../styles/About.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>About page</h1>
-    </div>
+    <section className="about">
+      <div className="head">
+        <h1>HELLO THERE! I'M JAN - A SOFTWARE DEVELOPER</h1>
+        <div className="underline" />
+      </div>
+      <div className="body">
+        <p>
+          I graduated in 2018 from the Northcoder full-time coding bootcamp in
+          Manchester.
+        </p>
+        <p>
+          I am highly motivated, self-driven, hard-working, learn new
+          technologies, eager to be a part of the team and become a valuable
+          employee of the company. When I'm not not programming, you can find me
+          playing video games or learning and reading more about software
+          development/engineering.
+        </p>
+      </div>
+    </section>
   );
 };
 
