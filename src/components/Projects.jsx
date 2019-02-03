1e2f1e2bfeedc4cd6cec80c3718d60dc802e9a08
Jan Machacek <janmachacek97@gmail.com>
2019-02-03 22:48:00 +0000

    add styling to projects container

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "../styles/Projects.css";
+import Project from "./Project";
+import projects from "../projects/projects.json";
 
 const Projects = () => {
   return (
     <section className="projects">
-      <div className="project-head">
+      <div className="projects-head">
         <h1>Projects</h1>
         <div className="underline" />
+      </div>
+      <div className="projects-container">
+        {console.log(projects)}
+        {projects.map((project, i) => (
+          <Project project={project} key={i} />
+        ))}
       </div>
     </section>
   );
