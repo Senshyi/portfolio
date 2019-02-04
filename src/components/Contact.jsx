68e792cee46d67ae20deffa80e5b5f308f6877cd
Jan Machacek <janmachacek97@gmail.com>
2019-02-04 14:13:47 +0000

    add contact content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <section>
-      <h1>Contact</h1>
+    <section className="contact">
+      <h1>CONTACT</h1>
+      <div className="underline" />
+      <h2>Get in touch with me!</h2>
+      <ul>
+        <li>github</li>
+        <li>linkedin</li>
+      </ul>
+      <p>
+        <span>E-mail</span>: janmachacek97@gmail.com
+      </p>
     </section>
   );
 };
