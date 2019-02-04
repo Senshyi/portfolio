911317bd9a8b705073efa6b6f00ebffab932eee3
Jan Machacek <janmachacek97@gmail.com>
2019-02-04 16:04:48 +0000

    add contact component content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/Contact.css";
 
 const Contact = () => {
   return (
@@ -7,8 +8,24 @@ const Contact = () => {
       <div className="underline" />
       <h2>Get in touch with me!</h2>
       <ul>
-        <li>github</li>
-        <li>linkedin</li>
+        <li>
+          <a
+            href="https://github.com/JanMach97"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-github" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/jan-machacek-a10148164/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-linkedin" />
+          </a>
+        </li>
       </ul>
       <p>
         <span>E-mail</span>: janmachacek97@gmail.com
