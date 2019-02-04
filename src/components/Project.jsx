4d347c949987b83d88f8ca776e14ea9fc462c784
Jan Machacek <janmachacek97@gmail.com>
2019-02-04 02:51:30 +0000

    add project card

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import "../styles/Project.css";
 
-const Project = () => {
+const Project = props => {
+  const { project } = props;
+  const img = require(`../images/${project.img}`);
   return (
-    <div>
-      <h1>Project</h1>
+    <div className="project">
+      <div className="meta">
+        <div className="photo" style={{ backgroundImage: `url(${img})` }} />
+        <ul className="details">
+          <li className="tech">tech</li>
+          <li>test</li>
+          <li>test2</li>
+        </ul>
+      </div>
+      <div className="description">
+        <h1>{project.name}</h1>
+        <p>{project.description}</p>
+      </div>
     </div>
   );
 };
